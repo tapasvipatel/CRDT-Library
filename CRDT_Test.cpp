5132a0a742074786d563ee7839f9b2d270e2a1da
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2021-09-30 01:11:34 -0400

    Rough Draft on CRDTs

---
+++ CRDT_Test.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//To do: Implement gCounter using key,value
+class g_Counter
+{
+public:
+    //Database
+    unordered_map<int, int> m;
+    //id
+    int id;
+
+    g_counter()
+    {
+        id = 0;
+    }
+
+    //Give each object unique id
+    g_Counter(int id)
+    {
+        this->id = id;
+    }
+
+}
+
+main()
+{
+    cout << "Hello World";
+    return 0;
+}
