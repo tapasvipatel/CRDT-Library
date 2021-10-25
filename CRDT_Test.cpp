e7e2946011ccd1814051e84c6cca4fe3717ba9c6
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2021-10-25 00:56:50 -0400

    Done Implementation of G_Counters

--- CRDT_Test.cpp
+++ CRDT_Test.cpp
@@ -16,9 +16,6 @@ Funtion 2 Objective --> Request the current value
 
 
 
-
-
-
 */
 
 class g_Counter
@@ -29,21 +26,62 @@ public:
     //id
     int id;
 
-    g_counter()
-    {
-        id = 0;
+    g_Counter(){
     }
-
-    //Give each object unique id
-    g_Counter(int id)
-    {
+     //Give each object unique id
+    g_Counter(int id){
         this->id = id;
     }
 
-}
+    g_Counter increment(int val = 1){ //the value represents how much we want to increment by // add function
+        g_Counter curr;
+        m[id]+=val;
+        curr.m[id]=m[id];
+        return curr;
+    }
+
+    void join(g_Counter replica){   //merge 
+        for (auto i: replica.m){
+            m[i.first] = max(i.second,m[i.first]);
+        }
+    }
+    int get_curr_val(){  // query lookup
+        return m[id];
+    }
+
+    int get_total_val(){
+        int res = 0;
+        for (auto i: m){
+            res+=i.second;
+        }
+        return res;
+    }
+
+    void print(){
+        cout << "GCounter: (";
+        for (auto i: m){
+            cout << i.first << "->" << i.second;
+        }
+        cout << ")";
+    }
+};
 
 main()
 {
-    cout << "Hello World";
-    return 0;
+    g_Counter replicaA(0);
+    g_Counter replicaB(1);
+    g_Counter replicaC;
+    g_Counter replicaD;
+    replicaC.join(replicaA.increment());
+    replicaC.join(replicaA.increment(7));
+    replicaD.join(replicaB.increment());
+    replicaD.join(replicaB.increment(5));
+    
+    cout << "Here Replica A and Replica C should converge" << endl;
+    replicaC.print();
+    replicaA.print();
+    cout << endl;
+    cout << "Here Replica D and Replica B should converge" << endl;
+    replicaD.print();
+    replicaB.print();
 }
