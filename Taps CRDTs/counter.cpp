bb92ddea2d02b59871e745af27781b94b2ca419a
Tapasvi <tapasvi.patel@mail.utoronto.ca>
2021-12-16 11:17:47 -0500

    Added metadata to store num increments and num decrements for counter CRDT.

--- Taps CRDTs/counter.cpp
+++ Taps CRDTs/counter.cpp
@@ -1,3 +1,27 @@
+/*
+    Copyright (c) 2021 [Tapasvi Patel, Rushab Roihan, Vishwa Gandhi, Winston Sun]
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the Software
+    is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "counter.hpp"
+
 namespace operation
 {
 
@@ -5,18 +29,22 @@ template<typename T>
 counter<T>::counter()
 {
     this->payload = T();
+    this->num_increments = 0;
+    this->num_decrements = 0;
 }
 
 template<typename T>
 counter<T>::counter(T payload)
 {
     this->payload = payload;
+    payload > 0 ? this->num_increments = payload : this->num_decrements = payload;
 }
 
 template<typename T>
 counter<T>::counter(const counter<T>& rhs)
 {
     this->payload = rhs.payload;
+    rhs.payload > 0 ? this->num_increments = rhs.payload : this->num_decrements = rhs.payload;
 }
 
 template<typename T>
@@ -35,36 +63,26 @@ template<typename T>
 void counter<T>::increment()
 {
     this->payload++;
+    this->num_increments++;
 }
 
 template<typename T>
 void counter<T>::decrement()
 {
     this->payload--;
+    this->num_decrements++;
 }
 
 template<typename T>
 counter<T> counter<T>::operator+(const counter<T>& rhs)
 {
-    return counter<T>(this->payload += rhs.payload);
+    return counter<T>(this->payload + rhs.payload);
 }
 
 template<typename T>
 counter<T> counter<T>::operator-(const counter<T>& rhs)
 {
-    return counter<T>(this->payload -= rhs.payload);
-}
-
-template<typename T>
-counter<T> counter<T>::operator*(const counter<T>& rhs)
-{
-    return counter<T>(this->payload *= rhs.payload);
-}
-
-template<typename T>
-counter<T> counter<T>::operator/(const counter<T>& rhs)
-{
-    return counter<T>(this->payload /= rhs.payload);
+    return counter<T>(this->payload - rhs.payload);
 }
 
 /*
@@ -79,23 +97,11 @@ counter<int32_t> counter<int32_t>::operator%(const counter<T>& rhs)
 template<typename T>
 counter<T> counter<T>::operator^(const counter<T>& rhs)
 {
-    return counter<T>(this->payload /= rhs.payload);
-}
-
-template<typename T>
-counter<T> counter<T>::operator^(const counter<T>& rhs)
-{
     return counter<T>(this->payload ^ rhs.payload);
 }
 
 template<typename T>
-bool counter<T>::operator^(const counter<T>& rhs)
-{
-    return this->payload & rhs.payload;
-}
-
-template<typename T>
-bool counter<T>::operator^(const counter<T>& rhs)
+bool counter<T>::operator&(const counter<T>& rhs)
 {
     return this->payload & rhs.payload;
 }
@@ -115,7 +121,8 @@ bool counter<T>::operator~(const counter<T>& rhs)
 template<typename T>
 void counter<T>::operator=(const counter<T>& rhs)
 {
-    return this->payload = rhs.payload;
+    this->payload = rhs.payload;
+    rhs.payload - this->payload > 0 ? this->num_increments += rhs.payload - this->payload : this->num_decrements -= this->payload - rhs.payload;
 }
 
 template<typename T>
@@ -145,13 +152,13 @@ bool counter<T>::operator>=(const counter<T>& rhs)
 template<typename T>
 void counter<T>::operator++()
 {
-    this->payload++;
+    this->increment();
 }
 
 template<typename T>
 void counter<T>::operator--()
 {
-    this->payload--;
+    this->decrement();
 }
 
 template<typename T>
@@ -170,24 +177,14 @@ template<typename T>
 void counter<T>::operator+=(const counter<T>& rhs)
 {
     this->payload += rhs.payload;
+    rhs.payload > 0 ? this->num_increments += rhs.payload : this->num_decrements += rhs.payload;
 }
 
 template<typename T>
 void counter<T>::operator-=(const counter<T>& rhs)
 {
     this->payload -= rhs.payload;
-}
-
-template<typename T>
-void counter<T>::operator/=(const counter<T>& rhs)
-{
-    this->payload /= rhs.payload;
-}
-
-template<typename T>
-void counter<T>::operator*=(const counter<T>& rhs)
-{
-    this->payload *= rhs.payload;
+    rhs.payload > 0 ? this->num_decrements += rhs.payload : this->num_increments += rhs.payload;
 }
 
 }
--- Taps CRDTs/counter.hpp
+++ Taps CRDTs/counter.hpp
@@ -1,11 +1,38 @@
+/*
+    Copyright (c) 2021 [Tapasvi Patel, Rushab Roihan, Vishwa Gandhi, Winston Sun]
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the Software
+    is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef __COUNTER_H__
 #define __COUNTER_H__
 
+/*
+* template class for CRDT counter
+*/
 template<typename T=int32_t>
 class counter
 {
 private:
     T payload;
+    int32_t num_increments;
+    int32_t num_decrements;
 protected:
     T query();
     void increment();
@@ -18,8 +45,6 @@ public:
 
     counter<T> operator+(const counter<T>& rhs);
     counter<T> operator-(const counter<T>& rhs);
-    counter<T> operator*(const counter<T>& rhs);
-    counter<T> operator/(const counter<T>& rhs);
     counter<T> operator%(const counter<T>& rhs);
     counter<T> operator^(const counter<T>& rhs);
     bool operator&(const counter<T>& rhs);
@@ -36,8 +61,6 @@ public:
     bool operator!=(const counter<T>& rhs);
     void operator+=(const counter<T>& rhs);
     void operator-=(const counter<T>& rhs);
-    void operator/=(const counter<T>& rhs);
-    void operator*=(const counter<T>& rhs);
 };
 
 #endif  // __COUNTER_H__
