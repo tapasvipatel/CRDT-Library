461cdf679823d9119b6871a6cdeabf8de0d240c5
Tapasvi <tapasvi.patel@mail.utoronto.ca>
2021-12-17 14:38:48 -0500

    Added build options for library.

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TESTING=''
+
+while [[ $# -gt 0 ]]; do
+	key="$1"
+	
+	case $key in
+		-testing)
+		TESTING=1
+		shift
+		;;
+	esac
+done
+
+rm -Rf build
+mkdir build
+cd build
+
+cmake_flags=''
+
+if [ $TESTING -eq 1 ]; then
+	cmake_flags=${cmake_flags}' -DBUILD_TESTING=1'
+fi
+
+cmake $cmake_flags ../ && make clean && make
